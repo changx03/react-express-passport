0364cde648708508a92a756f40fcc11c7e1ea514
Luke Chang <luke.x.chang@gmail.com>
2017-09-21 14:38:21 +1200

    fixed form submit

--- src/modules/register.tsx
+++ src/modules/register.tsx
@@ -4,7 +4,7 @@ import * as Express from "express";
 export class RegisterView extends React.Component<{}, any> {
     render() {
         return (
-            <form action="register" method="post">
+            <form name="register-form" action="register" method="post">
                 <div className="form-group">
                     <label htmlFor="loginName">User name</label>
                     <input
@@ -13,6 +13,7 @@ export class RegisterView extends React.Component<{}, any> {
                         id="loginName"
                         name="name"
                         defaultValue="Test Name"
+                        required
                     />
                     <label htmlFor="loginEmail">Email</label>
                     <input
@@ -23,6 +24,7 @@ export class RegisterView extends React.Component<{}, any> {
                         placeholder="Enter email"
                         name="email"
                         defaultValue="test@blabla.com"
+                        required
                     />
                     <label htmlFor="loginPassword">Password</label>
                     <input
@@ -32,6 +34,7 @@ export class RegisterView extends React.Component<{}, any> {
                         placeholder="Password"
                         name="password"
                         defaultValue="checkcheck1"
+                        required
                     />
                     <label htmlFor="loginPasswordConfirm">Confirm password</label>
                     <input
@@ -41,12 +44,28 @@ export class RegisterView extends React.Component<{}, any> {
                         placeholder="Password"
                         name="password2"
                         defaultValue="checkcheck1"
+                        required
                     />
-                    <button type="submit" className="btn btn-primary">
+                    <button type="button" className="btn btn-primary" onClick={this.onClick}>
                         Join
           </button>
                 </div>
             </form>
         );
+    }
+
+    validatForm = (): boolean => {
+        if(document.forms["register-form"]["password"].value !== document.forms["register-form"]["password2"].value) {
+            console.log("Passwords must match!");
+            return false;
+        } else { return true; }
+    }
+
+    onClick = (e) => {
+        e.preventDefault();
+        let validation = this.validatForm();
+        if(validation) {
+            document.forms["register-form"].submit();
+        }
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,60 +3,64 @@ var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  context: __dirname,
-  entry: ["./src/app.tsx"],
-  output: {
-    path: Path.resolve(__dirname, "build"),
-    filename: "[name].bundle.js"
-    // publicPath: "/dist"
-  },
-  devtool: "cheap-source-map",
-  module: {
-    loaders: [
-      {
-        test: /\.(ts|tsx)$/,
-        loaders: ["awesome-typescript-loader"]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        use: ["babel-loader"],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ExtractTextPlugin.extract({
-          use: ["css-loader", "sass-loader"]
+    context: __dirname,
+    entry: ["./src/app.tsx"],
+    output: {
+        path: Path.resolve(__dirname, "build"),
+        filename: "[name].bundle.js"
+        // publicPath: "/dist"
+    },
+    devtool: "inline-source-map",
+    module: {
+        loaders: [{
+                test: /\.(ts|tsx)$/,
+                loaders: ["awesome-typescript-loader"]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "sass-loader"]
+                })
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".jsx", ".js", ".tsx", ".ts"]
+    },
+    externals: {
+        jquery: "jQuery",
+        $: "jQuery"
+    },
+    plugins: [
+        new CleanWebpackPlugin(["build"]),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"development"',
+        }),
+        new ExtractTextPlugin("[name].css"),
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            cache: true
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: false,
+            mangle: false,
+            beautify: true,
+            comments: true,
+            sourceMap: true
+        }),
+        new BrowserSyncPlugin({
+            host: "localhost",
+            port: 3000,
+            proxy: "http://localhost:3283/",
+            files: ["build/*.html"]
         })
-      }
     ]
-  },
-  resolve: {
-    extensions: [".jsx", ".js", ".tsx", ".ts"]
-  },
-  externals: {
-    jquery: "jQuery",
-    $: "jQuery"
-  },
-  plugins: [
-    new ExtractTextPlugin("[name].css"),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      cache: true
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: false,
-      mangle: false,
-      beautify: true,
-      comments: true,
-      sourceMap: true
-    }),
-    new BrowserSyncPlugin({
-      host: "localhost",
-      port: 3000,
-      proxy: "http://localhost:3283/",
-      files: ["build/*.html"]
-    })
-  ]
 };
